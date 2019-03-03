8ac1bac9319efdc0f704494e4a83e0b4b89f0824
Rcmaniac25 <rcmaniac25@hotmail.com>
2019-03-03 14:44:55 -0500

    Print materials tables to console

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ def util_dict_values(d):
 	# https://stackoverflow.com/a/3294899/492347 (see the responses below the link)
 	return d.values()
 
+def util_dict_keys(d):
+	# https://stackoverflow.com/a/3294899/492347 (see the responses below the link)
+	return d.keys()
+
 def get_chroma_materials_path():
 	#TODO
 	path = "C:/Users/{0}/AppData/Roaming/Chroma/materials-2.yml"
@@ -21,6 +25,44 @@ def has_settings(mat, typ):
 			if combo["heatFactor"] != 0 and combo["compressionFactor"] != 0:
 				return True
 	return False
+
+def get_palette_type_name_mod(typ):
+	# Expects this is being used with the format "Palette{get_palette_type_name_mod(type)}"
+	if typ == "P":
+		return "/Palette+"
+	elif typ == "SC":
+		return " 2"
+	elif typ == "SCP":
+		return " 2 Pro"
+	return "<unknown typ {0}>".format(typ)
+
+def print_row(mat_merge_key, mat_merge_value, mat_merge_value_reverse, mat_gen, typ):
+	# m1 == m2 : only one set of values
+	# m1[combo][m2] : m1 = ingo, m2 = outgo
+	# m2[combo][m1] : m1 = outgo, m2 = ingo
+
+	def print_P(key, value):
+		print("{0} | Heat Factor: {1}, Compression Factor: {2}, Reverse Splicing: {3}".format(key, value["heatFactor"], value["compressionFactor"], value["reverse"]))
+
+	def print_SCx(key, value):
+		print("{0} | Heat Factor: {1}, Compression Factor: {2}, Cooling Factor: {3}, Reverse Splicing: {4}".format(key, value["heatFactor"], value["compressionFactor"], value["coolingFactor"], value["reverse"]))
+
+	if mat_merge_value["heatFactor"] != 0 and mat_merge_value["compressionFactor"] != 0:
+		if mat_merge_key == mat_gen:
+			if typ == "P":
+				print_P(mat_merge_key, mat_merge_value)
+			else:
+				print_SCx(mat_merge_key, mat_merge_value)
+		else:
+			combo1 = "{0} (ingoing) to {1} (outgoing)".format(mat_gen, mat_merge_key)
+			combo2 = "{0} (ingoing) to {1} (outgoing)".format(mat_merge_key, mat_gen)
+			do_combo2 = mat_merge_value_reverse["heatFactor"] != 0 and mat_merge_value_reverse["compressionFactor"] != 0
+			if typ == "P":
+				print_P(combo1, mat_merge_value)
+				if do_combo2: print_P(combo2, mat_merge_value_reverse)
+			else:
+				print_SCx(combo1, mat_merge_value)
+				if do_combo2: print_SCx(combo2, mat_merge_value_reverse)
 
 if __name__ == "__main__":
 	path = get_chroma_materials_path()
@@ -36,11 +78,17 @@ if __name__ == "__main__":
 		if has_settings(mat_val, palette_type):
 			material_names.append(mat_key)
 
-	# m1 == m2 : only one set of values
-	# m1[combo][m2] : m1 = ingo, m2 = outgo
-	# m2[combo][m1] : m1 = outgo, m2 = ingo
+	for mat_for_gen in material_names:
+		print("# {0}\n\nFor Palette{1}\n".format(mat_for_gen, get_palette_type_name_mod(palette_type)))
 
-	print(material_names)
+		print("## Splices\n\nMaterial | Values\n-------- | ------")
+		for mat_for_merg in material_names:
+			mat_for_merg_value = materials["matrix"][palette_type][mat_for_gen]["combinations"][mat_for_merg] # mat_for_gen = ingo, mat_for_merg = outgo
+			mat_for_merg_value_reversed = materials["matrix"][palette_type][mat_for_merg]["combinations"][mat_for_gen] # mat_for_gen = outgo, mat_for_merg = ingo
+
+			print_row(mat_for_merg, mat_for_merg_value, mat_for_merg_value_reversed, mat_for_gen, palette_type)
+
+		print("")
 
 # Process
 # 1. Convert from "materials" file that Chroma generates into a bunch of MD files for a Github page
